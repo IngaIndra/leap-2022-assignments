767f755161046fe0aad4300086e454ac3949f302
Indra.U <indra.ulziibadrakh@example.com>
2022-12-08 18:41:03 +0800

    Object

--- JS Lessons/Javascript/JS Array 12.5/array.js
+++ JS Lessons/Javascript/JS Array 12.5/array.js
@@ -1,10 +1,1 @@
 console.log("Hello");
-
-const name = 
-
-let sadad = 
-let asdads = 
-let = asdasdas =
-aasdasd = ;
-
-const name = 
--- JS Lessons/Javascript/JS Object 12.8/objects.js
+++ JS Lessons/Javascript/JS Object 12.8/objects.js
@@ -57,7 +57,7 @@ console.log(
   `Niit:${childrenCount} huuhed, ${youth} zaluuchuud, ${adult} dund nasnii humuus, ${elder} ahmad nasnii humuus baina`
 );
 
-//Age range bagshiin uurchlulttei
+//Age range bagshiin uurchilsun
 // function CountByFieldValueRange(list, key, min, max) {
 //   let count = 0;
 //   for (let i = 0; i < list.length; i++) {
@@ -80,4 +80,129 @@ console.log(
 //By grade
 // const totalA = CountByFieldValueRange(data, "grade", 90, 101);
 
-//Random str hussen str utga-s haidag function
+//HW-SIMPLE OBJECT
+let Dog = {
+  breed: "Corgi",
+  name: "June",
+  color: "Yellow",
+  age: 2.5,
+  lovableQuality: "Big ears",
+};
+console.log(
+  `${Dog.name}is a ${Dog.breed} whose color is ${Dog.color} and he is now ${Dog.age} years old. His lovable quality is his ${Dog.lovableQuality}`
+);
+
+//HW-MERN OBJECT
+const users = [
+  {
+    name: "Alex",
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 45,
+    isLoggedIn: false,
+    points: 30,
+  },
+  {
+    name: "Job",
+    email: "job@job.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  {
+    name: "Brook",
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 39,
+    isLoggedIn: true,
+    points: 50,
+  },
+  {
+    name: "Daniel",
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 25,
+    isLoggedIn: false,
+    points: 40,
+  },
+  {
+    name: "John",
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  {
+    name: "Thomas",
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 18,
+    isLoggedIn: false,
+    points: 40,
+  },
+  {
+    name: "Paul",
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 40,
+  },
+];
+
+//Нэг хэрэглэгчийн объектын бүх утгыг авч хэвлэх.
+let user7 = users[6];
+console.log(user7);
+// console.log(users[6].name, users[6].email, users[6].skills, users[6].age, users[6].isLoggedIn, users[6].points);
+
+//Хэрэглэгчийн объектод олон ур чадвар эзэмшсэн хүнийг олоорой.
+let maxNum = 0;
+let user;
+for (let i = 0; i < users.length; i++) {
+  if (users[i].skills.length > maxNum) {
+    user = users[i].name; //neriig ni hadgalj bga
+    maxNum = users[i].skills.length;
+  }
+}
+console.log(user);
+
+//Нэвтэрсэн хэрэглэгчдийг тоолж, дараах объектоос 50-аас дээш оноо авсан хэрэглэгчдийг тоол.
+function getPoint(point, max) {
+  let count = {
+    logged: 0,
+    points: 0,
+  };
+  let result = [];
+  for (i = 0; i < point.length; i++) {
+    if (point[i].isLoggedIn) {
+      count.logged++;
+      if (point[i].points >= max) {
+        count.points++;
+        result.push(point[i]);
+      }
+    }
+  }
+  return result;
+}
+console.log(getPoint(users, 50));
+
+//Хэрэглэгчийн объектоос MERN ( MongoDB, Express, React, Node ) стек хөгжүүлэгч хүмүүсийг олоорой.
