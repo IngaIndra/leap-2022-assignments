1f17507d32c47fd9d242b38b22e32a7087a5c816
Indra.U <indra.ulziibadrakh@example.com>
2022-12-12 17:37:50 +0800

    random color

--- 12.12 Lesson 19/DOM/Main lesson/function.js
+++ 12.12 Lesson 19/DOM/Main lesson/function.js
@@ -42,19 +42,19 @@
 // myButton.addEventListener("click", changeBackgroundColor);
 
 //darahaar HTML-n ungu ter chigtee soligdoh
-// function getRandomNumber(min, max) {
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
-// function getRandomColor() {
-//   const green = getRandomNumber(0, 255);
-//   const red = getRandomNumber(0, 255);
-//   const blue = getRandomNumber(0, 255);
-//   return `rgb(${red}, ${green}, ${blue})`;
-// }
-// const htmlTag = document.querySelector("html");
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function getRandomColor() {
+  const green = getRandomNumber(0, 255);
+  const red = getRandomNumber(0, 255);
+  const blue = getRandomNumber(0, 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+const htmlTag = document.querySelector("html");
 
-// const myButton = document.getElementById("myBtn");
+const myButton = document.getElementById("myBtn");
 
-// myButton.addEventListener("click", function () {
-//   htmlTag.style.backgroundColor = getRandomColor();
-// });
+myButton.addEventListener("click", function () {
+  htmlTag.style.backgroundColor = getRandomColor();
+});
---
+++ 12.12 Lesson 19/DOM/Main lesson/randomColor.js
@@ -0,0 +1,17 @@
+//darahaar HTML-n ungu ter chigtee soligdoh
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function getRandomColor() {
+  const green = getRandomNumber(0, 255);
+  const red = getRandomNumber(0, 255);
+  const blue = getRandomNumber(0, 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+const htmlTag = document.querySelector("html");
+
+const myButton = document.getElementById("myBtn");
+
+myButton.addEventListener("click", function () {
+  htmlTag.style.backgroundColor = getRandomColor();
+});
