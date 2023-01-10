f88fe12c33d1835034d750c6f2e725f85ef32805
Indra.U <indra.ulziibadrakh@example.com>
2023-01-10 17:50:46 +0800

    snake

--- React Lessons/snake/src/App.js
+++ React Lessons/snake/src/App.js
@@ -11,7 +11,11 @@ const headColor = "#ff0000";
 const foodColor = "#ffff00";
 const speed = 200;
 
-function Tile({ x, y, isHead, isFood }) {
+function randNum(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function Tile({ x, y, isHead }) {
   const style = {
     top: y * tileHeight,
     left: x * tileWidth,
@@ -19,6 +23,19 @@ function Tile({ x, y, isHead, isFood }) {
     width: tileWidth,
     height: tileHeight,
     backgroundColor: isHead ? headColor : bodyColor,
+    display: x === undefined || y === undefined ? "none" : "block",
+  };
+  return <div style={style}></div>;
+}
+
+function Food({ x, y }) {
+  const style = {
+    top: y * tileHeight,
+    left: x * tileWidth,
+    position: "absolute",
+    width: tileWidth,
+    height: tileHeight,
+    backgroundColor: foodColor,
   };
   return <div style={style}></div>;
 }
@@ -26,7 +43,7 @@ function Tile({ x, y, isHead, isFood }) {
 function App() {
   const directions = ["Up", "Left", "Down", "Right"];
   const [counter, setCounter] = useState(0);
-  const [food, setFood] = useState([{ x: 5, y: 5 }]);
+  const [food, setFood] = useState({ x: 5, y: 5 });
   const [direction, setDirection] = useState("Down");
   const [snake, setSnake] = useState([
     { x: 0, y: 2 },
@@ -45,22 +62,33 @@ function App() {
     }
   };
 
-  // const showFood = () => {
-  //   let newFood = [...food];
-  //   const randNum = Math.round(Math.random() * ());
-  //   return randNum;
-  // };
+  const renewFood = () => {
+    setFood({ x: randNum(xCells - 1), y: randNum(yCells - 1) }); //randNum position dr bga gedgiig ni shalgad bish bval shineer hool uusgene
+  };
+
+  const isEaten = () => {
+    let eaten = false;
+    if (snake[0].x === food.x && snake[0].y === food.y) {
+      eaten = true;
+    }
+    return eaten;
+  };
+
+  const growSnake = () => {
+    const _snake = [...snake];
+    _snake.push({});
+    setSnake(_snake);
+  };
 
   const moveDown = () => {
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) => {
       if (index === 0) {
-        if (item.y > yCells - snake.length + 1) {
+        if (item.y === yCells - 1) {
           return { x: item.x, y: 0 };
         }
         return { x: item.x, y: item.y + 1 };
       }
-
       return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
     });
     setSnake(newSnake);
@@ -70,7 +98,7 @@ function App() {
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) => {
       if (index === 0) {
-        if (item.x > xCells - snake.length + 1) {
+        if (item.x === xCells - 1) {
           return { x: 0, y: item.y };
         }
         return { x: item.x + 1, y: item.y };
@@ -145,6 +173,10 @@ function App() {
       default:
         console.log("non binary direction");
     }
+    if (isEaten()) {
+      growSnake();
+      renewFood();
+    }
   }, [counter]);
 
   setTimeout(() => {
@@ -158,6 +190,7 @@ function App() {
         className="board"
         style={{ width: xCells * tileWidth, height: yCells * tileHeight }}
       >
+        <Food x={food.x} y={food.y} />
         {snake.map((item, index) => {
           const isHead = index === 0;
           return (
